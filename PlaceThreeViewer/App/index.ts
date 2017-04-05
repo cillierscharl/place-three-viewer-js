c24731a93e9ee023d4d050a1183cf2dd7308b238
Charl Cilliers <cilliers.charl@gmail.com>
2017-04-05 19:46:35 -0400

    Add static bmp in case reddit bmp gets archived

--- PlaceThreeViewer/App/index.ts
+++ PlaceThreeViewer/App/index.ts
@@ -8,7 +8,7 @@ class Index {
 
     LoadBitmap = () => {
         var _context = this;
-        var myRequest = new Request('http://localhost:3000/getbitmap');
+        var myRequest = new Request('http://localhost:3000/place.bmp');
 
         fetch(myRequest).then(function (response) {
             var arrayBuffer = response.arrayBuffer();
